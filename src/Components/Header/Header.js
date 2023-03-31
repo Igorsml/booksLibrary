62dbada9bb066372ba876def3ff91ccec091acc2
Igorsml <igorsmlff@gmail.com>
2023-03-31 12:11:22 +0300

    feat: counter

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,6 +1,5 @@
 import React from "react";
 import css from "./Header.module.css";
-import { BooksCounter } from "../BooksCounter/BooksCounter";
 import { ReactComponent as SvgLogo } from "../../assets/icons/Logo.svg";
 
 export class Header extends React.Component {
@@ -9,7 +8,6 @@ export class Header extends React.Component {
       <header className={css.headerContainer}>
         <SvgLogo className={css.headerLogo} />
         <h1 className={css.headerTitle}>Vsesoki</h1>
-        <BooksCounter />
       </header>
     );
   }
